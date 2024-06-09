20ce76ce071190b4e7e23cad2f6443a767789f08
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-10 01:50:30 +0400

    Added the styling for the next-video section

--- src/components/NextVideo/NextVideo.jsx
+++ src/components/NextVideo/NextVideo.jsx
@@ -1,25 +1,27 @@
 import { useState } from "react";
 import NextVideoList from "../NextVideoList/NextVideoList";
+import "./NextVideo.scss";
 
 function NextVideo ({id, videoData, onVideoClicked}) {
-  const [filterData, setFilterData] = useState(id);
+  const [originVideoData, setNewVideoData] = useState(videoData);
 
   function createNewVideoList(id){
     const newVideoData = videoData.filter((item) => item.id !== id);
+    setNewVideoData(newVideoData);
     console.log(newVideoData);
     newVideoData.map((videoElement) => {
         return(
             <aside>
                 <h2>Next videos</h2>
-                <NextVideoList key={newVideoData.id} id={newVideoData.id} imageSrc={newVideoData.image} title={newVideoData.title} channel={newVideoData.channel} onVideoClicked={displaySelectedVideo} />
+                <NextVideoList key={newVideoData.id} id={newVideoData.id} image={newVideoData.image} title={newVideoData.title} channel={newVideoData.channel} onVideoClicked={displaySelectedVideo} />
             </aside>
         )
     })
   }
 
   return (
-    <aside>
-        <h2>Next videos</h2>
+    <aside class="next-video">
+        <h2 class="next-video__header">Next videos</h2>
         {videoData.map((videoElement) => {
             function displaySelectedVideo (){
                 onVideoClicked(videoElement.id);
@@ -27,12 +29,14 @@ function NextVideo ({id, videoData, onVideoClicked}) {
             }
             if(videoElement.id !== id){
                 return (
-                    <article key={videoElement.id} onClick={displaySelectedVideo}>
-                        <img src={videoElement.image} />
-                        <h2>{videoElement.title}</h2>
-                        <h2>{videoElement.channel}</h2>
+                    <article class="next-video__list" key={videoElement.id} onClick={displaySelectedVideo}>
+                        <img class="next-video__list__image" src={videoElement.image} />
+                        <div class="next-video__list__details">
+                            <h2 class="next-video__list__details__text">{videoElement.title}</h2>
+                            <h2 class="next-video__list__details__text next-video__list__details__text--light">{videoElement.channel}</h2>
+                        </div>
                     </article>
-            )
+                )
             }
         })}
     </aside>
