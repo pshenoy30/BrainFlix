ed6629f8dd9d779744e094f198885c66f693b954
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-10 03:45:43 +0400

    Added the styling for comment form

--- src/components/CommentForm/CommentForm.jsx
+++ src/components/CommentForm/CommentForm.jsx
@@ -1,14 +1,20 @@
-import avatarImage from "../../assets/Images/Mohan-muruge.jpg"
+import avatarImage from "../../assets/Images/Mohan-muruge.jpg";
+import commentImage from "../../assets/Icons/add_comment.svg"
+import "./CommentForm.scss";
+
 function CommentForm (){
   return (
-    <form>
-        <div>
-            <img src={avatarImage} alt="avatar image"></img>
+    <form class="comment-form">
+        <div class="comment-form__left">
+            <img class="comment-form__left__image" src={avatarImage} alt="avatar image"></img>
         </div>
-        <div>
-            <h3>Join the converstation</h3>
-            <textarea placeholder="Add a new comment"></textarea>
-            <button type="submit">Comment</button>
+        <div class="comment-form__right">
+            <h3 class="comment-form__right__header">Join the converstation</h3>
+            <input placeholder="Add a new comment" class="comment-form__right__input" />
+            <button type="submit" class="comment-form__right__button">
+              <img class="comment-form__right__button__image" src={commentImage} />
+              Comment
+            </button>
         </div>
     </form>
   )
