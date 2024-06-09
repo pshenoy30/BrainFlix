375e17230a3c235f0f433c06ab6ffd46685ca103
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-10 02:11:47 +0400

    addedstyling to the next-video-list.jsx file

--- src/components/NextVideoList/NextVideoList.jsx
+++ src/components/NextVideoList/NextVideoList.jsx
@@ -1,9 +1,11 @@
 function NextVideoList ({id, image, title, channel, onVideoSelected}) {
   return (
-    <article key={id} onClick={onVideoSelected}>
-        <img src={image} />
-        <h2>{title}</h2>
-        <h2>{channel}</h2>
+    <article class="next-video__list" key={id} onClick={onVideoSelected}>
+        <img class="next-video__list__image" src={image} />
+        <div class="next-video__list__details">
+          <h2 class="next-video__list__details__text">{title}</h2>
+          <h2 class="next-video__list__details__text next-video__list__details__text--light">{channel}</h2>
+        </div>
     </article>
   )
 }
