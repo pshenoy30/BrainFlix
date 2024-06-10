f0c38a24a10ba0fa8bf38f5c60cf442ae7ffd932
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-10 05:28:46 +0400

    Added the styling for the video player

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,10 +1,12 @@
+import "./VideoPlayer.scss"
+
 function VideoPlayer ({videoData}) {
 
   const {image, video} = videoData;
   
   return (
     <>
-    <video controls poster={image} > 
+    <video className="video-player" controls poster={image} > 
         <source src={video} />
         Your browser doesnot support video
     </video>
