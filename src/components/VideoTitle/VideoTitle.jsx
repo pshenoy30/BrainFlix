4c761316edc63e53f9505089a6fa5193ec4c04c9
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-10 05:14:20 +0400

    Added styling for video title

--- src/components/VideoTitle/VideoTitle.jsx
+++ src/components/VideoTitle/VideoTitle.jsx
@@ -1,8 +1,10 @@
+import "./VideoTitle.scss";
+
 function VideoTitle({videoDataTitle}){
 
     return (
         <article>
-            <h1>{videoDataTitle}</h1>
+            <h1 className="video-title">{videoDataTitle}</h1>
             <hr></hr>
         </article>
     )
