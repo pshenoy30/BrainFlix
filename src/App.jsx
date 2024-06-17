3ce069be6136d10ea711a6cacbbc85bb349a6de4
Pratiksha Shenoy <pratikshashenoy@Pratikshas-MacBook-Pro.local>
2024-06-17 04:48:26 +0400

    Added a not founf page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import HomePage from "./pages/HomePage/HomePage.jsx"
 import UploadPage from "./pages/UploadPage/UploadPage.jsx"
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx"
 function App() {
 
   return (
@@ -9,6 +10,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/videos/:videoId" element={<HomePage />} />
         <Route path="/upload" element={<UploadPage />} />
+
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,7 @@
+function NotFoundPage () {
+  return (
+    <h1>Sorry, this page is not found</h1>
+  )
+}
+
+export default NotFoundPage;
